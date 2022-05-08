d4f05f010c89f81aee2558bfa26c1566d3a906d4
Mohammed Jewel <jewelnabin@gmail.com>
2022-05-08 21:44:25 +0600

    Include some text

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -17,8 +17,8 @@ const Banner = () => {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>Beautiful Earrings</h3>
+                        <p>Varitey High Demand Just For A Queen.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={500}>
@@ -28,8 +28,8 @@ const Banner = () => {
                         alt="Second slide"
                     />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>Sapphire Crystal </h3>
+                        <p>Ring Solitaire Princess</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -39,8 +39,8 @@ const Banner = () => {
                         alt="Third slide"
                     />
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3>Women's Tatty Devine Necklaces</h3>
+                        <p>Created to honor the rich potential in every woman, Tiffany Keys are a vibrant symbol of all that is possible.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
